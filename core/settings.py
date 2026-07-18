ee721369d04bb46088228c599d11fea56dea5e82
Siva-Rajan-R <siva967763@gmail.com>
2026-07-18 13:52:38 +0530

    fixed

--- core/settings.py
+++ core/settings.py
@@ -9,6 +9,10 @@ class SupplierSettings(BaseSettings):
     PG_DATABASE_URL:str
     ENVIRONMENT:EnvironmentEnum
     READ_DB_URL:str
+    RABBITMQ_HOST:str
+    RABBITMQ_PORT:int
+    RABBITMQ_LOGIN:str
+    RABBITMQ_PASSWORD:str
 
     model_config={
         'env_prefix':ENV_PREFIX,
