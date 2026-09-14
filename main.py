85514c496d243bdd5847545daa445311dc311b6c
Siva-Rajan-R <siva967763@gmail.com>
2026-09-15 01:35:22 +0530

    fixed

--- main.py
+++ main.py
@@ -19,14 +19,17 @@ async def supplier_service_lifespan(app:FastAPI):
         await init_pg_db()
         await init_read_db()
         print("[SUPPLIER SERVICE] ✅ Database & Read DB initialized. Ready for background tasks & exports.")
-        asyncio.create_task(worker())
+        app.state.worker_task = asyncio.create_task(worker())
         yield
 
     except Exception as e:
         ic(f"Error : Starting supplier service => {e}")
 
     finally:
-        ic("...Stoping supplier Servcie...")
+        ic("...Stopping supplier Service...")
+        if hasattr(app.state, "worker_task") and app.state.worker_task:
+            app.state.worker_task.cancel()
+            await asyncio.gather(app.state.worker_task, return_exceptions=True)
 
 debug=False
 openapi_url=None
--- messaging/worker.py
+++ messaging/worker.py
@@ -42,4 +42,10 @@ async def worker():
     shop_config_consumer = ShopConfigMsgQueueConsumer()
     await shop_config_consumer.consume()
 
-    await asyncio.Event().wait()
+    try:
+        await asyncio.Event().wait()
+    except asyncio.CancelledError:
+        pass
+    finally:
+        if rabbitmq_conn and not rabbitmq_conn.is_closed:
+            await rabbitmq_conn.close()
